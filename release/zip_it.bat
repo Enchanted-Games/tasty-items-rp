f47547792637bafb27ea6d89490e22bc805334b0
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-11-15 01:05:36 +0000

    update the build script

--- release/zip_it.bat
+++ release/zip_it.bat
@@ -1,4 +1,7 @@
-REM Copy pack files to the current directory
+@echo off
+set /p "file_version_num=Filename version number: "
+
+REM Copy pack files to the current directory temporarily
 copy /y ..\pack.png .
 copy /y ..\pack.mcmeta .
 copy /y ..\credits.txt .
@@ -6,4 +9,10 @@ REM Copy all files from ../Template/build/bin/Debug to the current directory
 xcopy /s /y /q ..\assets\* .\assets\
 
 REM Create a zip file named Tasty_Items_v.zip containing all files (except zip_it.bat) in the current directory
-"C:\Program Files\7-Zip\7z.exe" a zips/Tasty_Items_v.zip * -x!zip_it.bat -x!*.zip
+"C:\Program Files\7-Zip\7z.exe" a zips/Tasty_Items_v%file_version_num%.zip * -x!zip_it.bat -x!*.zip
+
+REM Remove temporarily copied files
+del .\pack.png
+del .\pack.mcmeta
+del .\credits.txt
+rmdir .\assets /s /q
